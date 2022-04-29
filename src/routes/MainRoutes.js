6c294793494709d5c40069c6e89a102de1dd85b3
badoalmaz <85788119+badoalmaz@users.noreply.github.com>
2022-04-29 10:29:44 +0600

    commit

---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import AboutUsPage from '../pages/AboutUsPage';
+import AuthPage from '../pages/AuthPage';
+import CartPage from '../pages/CartPage';
+import ContactUsPage from '../pages/ContactUsPage';
+import HomePage from '../pages/HomePage';
+import NotFoundPage from '../pages/NotFoundPage';
+import ProductDetailsPage from '../pages/ProductDetailsPage';
+import ProductsPage from '../pages/ProductsPage';
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    {
+      link: '/',
+      element: <HomePage />,
+      id: 1,
+    },
+    {
+      link: '/auth',
+      element: <AuthPage />,
+      id: 2,
+    },
+    {
+      link: '/about',
+      element: <AboutUsPage />,
+      id: 3,
+    },
+    {
+      link: '/products',
+      element: <ProductsPage />,
+      id: 4,
+    },
+    {
+      link: '/products/:id',
+      element: <ProductDetailsPage />,
+      id: 5,
+    },
+    {
+      link: '/cart',
+      element: <CartPage />,
+      id: 6,
+    },
+    {
+      link: '/contacts',
+      element: <ContactUsPage />,
+      id: 7,
+    },
+    {
+      link: '*',
+      element: <NotFoundPage />,
+      id: 8,
+    },
+  ];
+
+  return <div></div>;
+};
+
+export default MainRoutes;
