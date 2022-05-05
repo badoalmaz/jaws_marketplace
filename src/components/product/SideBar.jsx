ddf5c65b4fa549f515a41ac061032e54751614a1
badoalmaz <85788119+badoalmaz@users.noreply.github.com>
2022-05-05 11:24:42 +0600

    filtration

--- src/components/product/SideBar.jsx
+++ src/components/product/SideBar.jsx
@@ -3,10 +3,17 @@ import React, { useEffect, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { useSearchParams } from 'react-router-dom';
 
-const SideBar = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams);
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { useProducts } from '../../contexts/ProductContexProvider';
 
+const SideBar = () => {
+  const { fetchByParams } = useProducts();
+
+  const [searchParams, setSearchParams] = useSearchParams();
   const [search, setSearch] = useState(searchParams.get('q') || '');
 
   useEffect(() => {
@@ -33,6 +40,68 @@ const SideBar = () => {
           }}
           variant="standard"
         />
+
+        <Grid>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Type</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="all"
+              name="radio-buttons-group"
+              onChange={(e) => fetchByParams('type', e.target.value)}
+            >
+              <FormControlLabel value="all" control={<Radio />} label="all" />
+
+              <FormControlLabel
+                value="telephone"
+                control={<Radio />}
+                label="telephone"
+              />
+
+              <FormControlLabel
+                value="laptop"
+                control={<Radio />}
+                label="laptop
+              "
+              />
+              <FormControlLabel
+                value="watch"
+                control={<Radio />}
+                label="watch"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Price</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="all"
+            name="radio-buttons-group"
+            onChange={(e) => fetchByParams('price_lte', e.target.value)}
+          >
+            <FormControlLabel value="all" control={<Radio />} label="all" />
+
+            <FormControlLabel
+              value="100"
+              control={<Radio />}
+              label="less than 100"
+            />
+
+            <FormControlLabel
+              value="600"
+              control={<Radio />}
+              label="less than 600
+          "
+            />
+            <FormControlLabel
+              value="1300"
+              control={<Radio />}
+              label="less than 1300"
+            />
+          </RadioGroup>
+        </FormControl>
       </Paper>
     </Grid>
   );
--- src/contexts/ProductContexProvider.js
+++ src/contexts/ProductContexProvider.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { createContext, useContext, useReducer } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ACTIONS, JSON_API_PRODUCTS } from '../helpers/consts';
 
 export const productContext = createContext();
@@ -26,6 +27,9 @@ const reducer = (state = INIT_STATE, action) => {
 
 const ProductContexProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
+
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const getProducts = async () => {
     const { data } = await axios(
@@ -60,6 +64,21 @@ const ProductContexProvider = ({ children }) => {
     getProducts();
   };
 
+  //filter
+  const fetchByParams = async (query, value) => {
+    const search = new URLSearchParams(location.search);
+
+    if (value === 'all') {
+      search.delete(query);
+    } else {
+      search.set(query, value);
+    }
+    const url = `${location.pathname}?${search.toString()}`;
+    console.log(search.toString());
+    console.log(url);
+    navigate(url);
+  };
+
   const values = {
     products: state.products,
     productDetails: state.productDetails,
@@ -69,6 +88,8 @@ const ProductContexProvider = ({ children }) => {
     getProductDetails,
     deleteProduct,
     saveEditedProduct,
+
+    fetchByParams,
   };
 
   return (
