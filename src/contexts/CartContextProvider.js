0792660fdb0d8c0779f9de9c1c14ce89c2d69a82
badoalmaz <85788119+badoalmaz@users.noreply.github.com>
2022-05-06 11:08:06 +0600

    cart 2

--- src/contexts/CartContextProvider.js
+++ src/contexts/CartContextProvider.js
@@ -1,4 +1,6 @@
 import React, { createContext, useContext, useReducer } from 'react';
+import { CART } from '../helpers/consts';
+import { getCountProductsInCart } from '../helpers/functions';
 
 const cartContext = createContext();
 
@@ -8,8 +10,19 @@ export const useCart = () => {
 
 const INIT_STATE = {
   cart: JSON.parse(localStorage.getItem('cart')),
-  cartLength: 0,
+  cartLength: getCountProductsInCart(),
 };
+
+function reducer(state = INIT_STATE, action) {
+  switch (action.type) {
+    case CART.GET_CART:
+      return { ...state, cart: action.payload };
+    case CART.GET_CART_LENGTH:
+      return { ...state, cartLength: action.payload };
+    default:
+      return state;
+  }
+}
 
 const CartContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
